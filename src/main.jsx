d1f59b90037ccb3efd52bf713100bb2e2cd9e008
Sumitra Paul <sumitra.cse.just2020@gmail.com>
2023-12-11 19:18:13 +0600

    react router setup

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Root from './Layout/Root';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
+import Skills from './pages/Skills/Skills';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/skills",
+        element: <Skills></Skills>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <RouterProvider router={router} />
+  </React.StrictMode>,
+)
